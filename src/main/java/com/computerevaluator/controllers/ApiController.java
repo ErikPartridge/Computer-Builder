8a67a5f661632684cbf5c01a4d8d010b9ac3e0ff
Erik Partridge <erik.partridge@mail.mcgill.ca>
2016-07-01 11:40:03 -0400

    Rebuild

--- src/main/java/com/computerevaluator/controllers/ApiController.java
+++ src/main/java/com/computerevaluator/controllers/ApiController.java
@@ -11,7 +11,6 @@ public class ApiController{
 
     public static Response getResult(Request req, Response res){
         long id = Long.parseLong(req.url().split("\\/api\\/")[1]);
-        System.out.println(id);
         res.type("application/json");
         res.status(200);
         Result result = Result.find(id);
